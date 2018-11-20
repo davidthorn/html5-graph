33814b208d651ee71bad2c4b17ab44eb374363e8
David Thorn <david.thorn221278@googlemail.com>
2018-11-20 17:03:05 +0100

    added margin to the grid

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 class GridObject {
 
-    constructor(width, height , marginX , marginY)  {
+    constructor(width, height , marginX = 10 , marginY = 10)  {
         this.width = width;
         this.height = height;
-        this.marginX = marginX;
-        this.marginY = marginY;
+        this.margin = {
+            x: marginX,
+            y: marginY
+        }
     }
 
     centerX() {
@@ -31,8 +33,9 @@ class GraphObject {
     draw() {
         this.context.fillStyle = 'beige';
         this.context.fillRect(0,0, this.frame.width , this.frame.height);
-        this.line(this.frame.centerX() , 0 , this.frame.centerX() , this.frame.height);
-        this.line(0 , this.frame.centerY() , this.frame.width , this.frame.centerY());
+        console.log(this.frame.marginY);
+        this.line(this.frame.centerX() , this.frame.margin.y , this.frame.centerX() , this.frame.height - this.frame.margin.y); /// vertical line y axis
+        this.line(this.frame.margin.x , this.frame.centerY() , this.frame.width - this.frame.margin.x, this.frame.centerY()); /// horizontal line x axis
 
     }
 
